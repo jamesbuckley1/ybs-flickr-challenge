0681ce21f8c2c8454d9f3a0596773787787e06dc
James Buckley <jamespgbuckley@icloud.com>
2024-08-18 23:40:39 +0100

    Add project files

---
+++ ybs-flickr-challenge/ybs_flickr_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ybs_flickr_challengeApp.swift
+//  ybs-flickr-challenge
+//
+//  Created by James Buckley on 18/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ybs_flickr_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
